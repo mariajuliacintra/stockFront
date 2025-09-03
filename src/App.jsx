ea96f3106e502468d9ae909ff666a78ec1f670d3
HyagoGabriel <hyagogabriel3@gmail.com>
2025-09-03 15:59:13 -0300

    Atualizaçao da pagina  do editar perfil funcional e criaçao da pagina do AtualizarPerfil terminando a o delete

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import RecSenha from "./pages/RecSenha";
 import Perfil from "./pages/Perfil"; 
+import AtualizarPerfil from "./pages/AtualizarPerfil";
 import PageCards from "./pages/PageCards";
 import Equipments from "./pages/Equipments/Equipments";
 import Material from "./pages/Equipments/Material";
@@ -19,103 +20,111 @@ import DefaultLayout from "./components/layout/DefaultLayout";
 
 
 const theme = createTheme({
-  typography: {
-    fontFamily: "'Roboto Mono', monospace",
-  },
+  typography: {
+    fontFamily: "'Roboto Mono', monospace",
+  },
 });
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          {/* Rota da página inicial */}
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/login"
-            element={
-              <DefaultLayout>
-                <Login />
-              </DefaultLayout>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <DefaultLayout>
-                <Register />
-              </DefaultLayout>
-            }
-          />
-          <Route
-            path="/recsenha"
-            element={
-              <DefaultLayout>
-                <RecSenha />
-              </DefaultLayout>
-            }
-          />
-          <Route
-            path="/perfil"
-            element={
-              
-                <Perfil />
-              
-            }
-          />
-          <Route
-            path="/principal"
-            element={
-              <ProtectedRouter>
-                <PageCards />
-              </ProtectedRouter>
-            }
-          />
-          <Route
-            path="/equipamentos"
-            element={
-                <Equipments />
-            }
-          />
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          {/* Rota da página inicial */}
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/login"
+            element={
+              <DefaultLayout>
+                <Login />
+              </DefaultLayout>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <DefaultLayout>
+                <Register />
+              </DefaultLayout>
+            }
+          />
+          <Route
+            path="/recsenha"
+            element={
+              <DefaultLayout>
+                <RecSenha />
+              </DefaultLayout>
+            }
+          />
+          <Route
+            path="/perfil"
+            element={
+              
+                <Perfil />
+              
+            }
+          />
+          <Route
+            path="/AtualizarPerfil"
+            element={
+              
+                <AtualizarPerfil />
+              
+            }
+          />
+          <Route
+            path="/principal"
+            element={
+              <ProtectedRouter>
+                <PageCards />
+              </ProtectedRouter>
+            }
+          />
+          <Route
+            path="/equipamentos"
+            element={
+                <Equipments />
+            }
+          />
 
-          <Route
-            path="/Material"
-            element={
-                <Material />
-            }
-          />
+          <Route
+            path="/Material"
+            element={
+                <Material />
+            }
+          />
 
-          <Route
-            path="/Diversos"
-            element={
-                <Others />
-            }
-          />
+          <Route
+            path="/Diversos"
+            element={
+                <Others />
+            }
+          />
 
-          <Route
-            path="/Produtos"
-            element={
-                <Products />
-            }
-          />
+          <Route
+            path="/Produtos"
+            element={
+                <Products />
+            }
+          />
 
-          <Route
-            path="/materia-prima"
-            element={
-                <Raw_material />
-            }
-          />
+          <Route
+            path="/materia-prima"
+            element={
+                <Raw_material />
+            }
+          />
 
-          <Route
-            path="/Ferramenta"
-            element={
-                <Tools />
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+          <Route
+            path="/Ferramenta"
+            element={
+                <Tools />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
