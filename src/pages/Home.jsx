71fddd74d22e62a389b5de0f1d76f154d15c811c
mariajuliacintra <mariajuliacintra31@gmail.com>
2025-11-17 10:32:53 -0300

    Atualização do link para o APK

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
 
   const downloadApk = () => {
     window.location.href =
-      "https://expo.dev/accounts/mariafernandacintra/projects/senai-estoque-v1/builds/01fc6d25-9768-4cf7-912b-2332226834f0";
+      "https://expo.dev/accounts/mariafernandacintra/projects/senai-estoque-v1/builds/47c2ea8e-906f-4ff1-bbd1-3039c6b969cd";
   };
 
   return (
